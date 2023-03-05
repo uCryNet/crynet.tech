36cc0ae59432e43e2510d6df98e3a6e186a3625d
crynet <ucrynet@gmail.com>
2023-03-05 22:39:05 +0200

    build

--- client/src/config/api.ts
+++ client/src/config/api.ts
@@ -3,3 +3,4 @@ import axios from 'axios';
 
 axios.defaults.baseURL = process.env.VUE_APP_API
 axios.defaults.withCredentials = true
+axios.defaults.headers.common[ 'X-Requested-With' ] = 'XMLHttpRequest';
